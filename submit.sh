2d38d2d4f9cfc879d04a265b72ece13005f04384
pabloleonbarriga <pabloleonbarriga@gmail.com>
2024-12-06 00:14:00 +0100

    Modify output folder in submit.sh

--- submit.sh
+++ submit.sh
@@ -17,8 +17,8 @@
 #BSUB -R "rusage[mem=30GB]"
 
 #BSUB -W 12:00 
-#BSUB -o output_2/OUTPUT_FILE%J.out 
-#BSUB -e output_2/OUTPUT_FILE%J.err
+#BSUB -o output/OUTPUT_FILE%J.out 
+#BSUB -e output/OUTPUT_FILE%J.err
 
 source "/zhome/d8/f/203934/ddpm/deep_learning_ddpm_group_7/.venv/bin/activate"
 python -m tools.train_ddpm
